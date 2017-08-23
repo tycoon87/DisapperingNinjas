49a34728a54bcad5d16d255c392118fea9cb8549
tycoon87 <tyson.woodruff@outlook.com>
2017-08-22 19:27:34 -0700

    the ninjas fell poped into the multiverse

---
+++ DisappearingNinjas.js
@@ -0,0 +1,80 @@
+$( '.ninja1' ).click(function() {
+    $( '.ninja1' ).fadeOut( "slow", function(){
+        
+    })
+})
+$( '.ninja2' ).click(function() {
+    $( '.ninja2' ).fadeOut( "slow", function(){
+        
+    })
+})
+$( '.ninja3').click(function() {
+    $( '.ninja3').fadeOut( "slow", function(){
+        
+    })
+})
+$( '.ninja4').click(function() {
+    $( '.ninja4').fadeOut( "slow", function(){
+        
+    })
+})
+$( '.ninja5').click(function() {
+    $( '.ninja5').fadeOut( "slow", function(){
+        
+    })
+})
+$( '.ninja6' ).click(function() {
+    $( '.ninja6' ).fadeOut( "slow", function(){
+        
+    })
+})
+$( '.ninja7').click(function() {
+    $('.ninja7').fadeOut( "slow", function(){
+        
+    })
+})
+$( '.ninja8').click(function() {
+    $( '.ninja8').fadeOut( "slow", function(){
+        
+    })
+})
+$( '.button' ).click(function(){
+    $( '.ninja1' ).fadeIn(function(){
+        
+    })
+})
+$( '.button' ).click(function(){
+    $( '.ninja2' ).fadeIn(function(){
+        
+    })
+})
+$( '.button' ).click(function(){
+    $( '.ninja3' ).fadeIn(function(){
+        
+    })
+})
+$( '.button' ).click(function(){
+    $( '.ninja4' ).fadeIn(function(){
+        
+    })
+})
+$( '.button' ).click(function(){
+    $( '.ninja5' ).fadeIn(function(){
+        
+    })
+})
+$( '.button' ).click(function(){
+    $( '.ninja6' ).fadeIn(function(){
+        
+    })ss
+})
+$( '.button' ).click(function(){
+    $( '.ninja7' ).fadeIn(function(){
+        
+    })
+})
+$( '.button' ).click(function(){
+    $( '.ninja8' ).fadeIn(function(){
+        
+    })
+})
